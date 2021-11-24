c5ca2c94841788fa5e0188289839dbe9b1ddb16c
DD <D>
2021-11-24 22:45:51 +0200

    Module3

---
+++ module3/centos7/docker_install.sh
@@ -0,0 +1,6 @@
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce -y
+sudo systemctl enable docker
+sudo usermod -aG docker $(whoami)
+sudo systemctl start docker
---
+++ module3/ubuntu/run.ps1
@@ -0,0 +1,7 @@
+vagrant init ubuntu/focal64
+$file = '.\Vagrantfile'
+$find = '  # config.vm.provision "shell", inline: <<-SHELL'
+$replace = '  config.vm.provision "shell", path: "docker_install.sh"'
+(Get-Content $file).replace($find, $replace) | Set-Content $file
+vagrant up
+vagrant ssh
